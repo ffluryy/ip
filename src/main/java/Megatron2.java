bc6f64acde49faebdd28c93b2529b0bb3f2c25a9
ffluryy <kaijie.chong@gmail.com>
2026-02-02 10:58:45 +0800

    Added echo functionality, Megatron shouts it back

--- src/main/java/Megatron2.java
+++ src/main/java/Megatron2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Megatron2 {
     public static void greet(String botName) {
         System.out.println("Hello! I'm " + botName + ".");
@@ -7,9 +9,25 @@ public class Megatron2 {
     public static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    public static void echo(String inputString) {
+        System.out.println(inputString.toUpperCase());
+    }
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         String botName = "Megatron 2";
+        String exitWord = "bye";
         greet(botName);
+        boolean exit = false;
+        while (!exit) {
+            String userInput = scan.nextLine();
+            if (userInput.equals(exitWord)) {
+                exit = true;
+            } else {
+                echo(userInput);
+            }
+        }
+
         bye();
     }
 }
